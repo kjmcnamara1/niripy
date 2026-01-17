532349a21aa94a4a84ca541cbce855076d41765e
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-01-16 19:01:56 -0500

    feat(instance): add Instance class

--- src/niripy/__init__.py
+++ src/niripy/__init__.py
@@ -1,2 +1,9 @@
-def hello() -> str:
-    return "Hello from niripy!"
+"""
+Niripy
+===
+
+Niripy is a python library which provides bindings for the `Niri <https://yalter.github.io/niri/>` wayland compositor.
+"""
+
+from niripy.instances import Instance
+
