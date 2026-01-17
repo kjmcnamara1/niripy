562e9f4fee71dfbea172da828fc5cdd210fbefb9
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-01-16 19:35:14 -0500

    test(socket): simple socket test

--- src/niripy/__init__.py
+++ src/niripy/__init__.py
@@ -7,3 +7,10 @@ Niripy is a python library which provides bindings for the `Niri <https://yalter
 
 from niripy.instances import Instance
 
+if __name__ == "__main__":
+    niri = Instance()
+    niri.socket.connect()
+    print(niri)
+    print(niri.socket)
+    print(niri.socket._socket)
+    print(niri.socket._path_to_socket)
