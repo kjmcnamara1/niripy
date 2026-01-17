07582123956a27f47f638e734812f245c3dba824
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-01-17 15:02:15 -0500

    feat(instance): add get_windows and get_workspaces

--- src/niripy/__init__.py
+++ src/niripy/__init__.py
@@ -7,10 +7,11 @@ Niripy is a python library which provides bindings for the `Niri <https://yalter
 
 import json
 
+from icecream import ic
+
 from niripy.instances import Instance
 
 if __name__ == "__main__":
     niri = Instance()
-    request = {"Windows": None}
-    print(niri.socket.send_command(json.dumps(request) + "\n"))
-    print(niri.socket.send_command('{"FocusedWindow": null}\n'))
+    ic(niri.get_windows())
+    ic(niri.get_workspaces())
--- src/niripy/instances.py
+++ src/niripy/instances.py
@@ -1,6 +1,36 @@
+import json
+from typing import TypeVar
+
+from niripy.models import ModelWithInstance, Window, Workspace
 from niripy.sockets import Socket
+
+T = TypeVar("T", bound=ModelWithInstance)
 
 
 class Instance:
+    socket: Socket
+
     def __init__(self):
-        self.socket: Socket = Socket()
+        self.socket = Socket()
+
+    def _create_model_with_instance(self, model: type[T], data: dict):
+        m = model(**data)
+        m._instance = self
+        return m
+
+    def get_windows(self) -> list[Window]:
+        windows: list[dict] = json.loads(
+            self.socket.send_command('{"Windows": null}\n')
+        )["Ok"]["Windows"]
+        # print(json.dumps(windows_data, indent=4))
+        return [self._create_model_with_instance(Window, w) for w in windows]
+
+    def get_workspaces(self) -> list[Workspace]:
+        workspaces: list[dict] = json.loads(
+            self.socket.send_command('{"Workspaces": null}\n')
+        )["Ok"]["Workspaces"]
+        return [self._create_model_with_instance(Workspace, ws) for ws in workspaces]
+
+    # def action(self, arguments: list[str]):
+    #     response = self.socket.send_command("action", flags=["-j"], args=arguments)
+    #     return response if response != "ok" else None
