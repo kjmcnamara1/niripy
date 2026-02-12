806274149bf2f98cb1e55851337f7ca50f5bb591
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-02-11 21:17:43 -0500

    fix(init): remove junk

--- src/niripy/__init__.py
+++ src/niripy/__init__.py
@@ -6,12 +6,4 @@ Niripy is a python library which provides bindings for the `Niri
 <https://yalter.github.io/niri/>` wayland compositor.
 """
 
-from icecream import ic
-
 from niripy.instances import Instance
-
-if __name__ == "__main__":
-    niri = Instance()
-
-    o = niri.get_focused_output()
-    _ = ic(o.workspaces)
